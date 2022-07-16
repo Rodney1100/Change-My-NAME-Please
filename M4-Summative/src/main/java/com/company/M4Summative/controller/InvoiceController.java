df75fcb4e5ce18075e3e802eedecab1c145a8309
rodney1100 <rodney.plancher@gmail.com>
2022-07-16 00:37:18 -0400

    need controllers for invoice, processingfee, salesTax, test for them all and connect front end

---
+++ M4-Summative/src/main/java/com/company/M4Summative/controller/InvoiceController.java
@@ -0,0 +1,18 @@
+package com.company.M4Summative.controller;
+
+import com.company.M4Summative.model.Invoice;
+import com.company.M4Summative.repository.InvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequestMapping(value = "/games")
+@RestController
+public class InvoiceController {
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
+}
